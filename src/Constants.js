a6fa766c286c0e56610348b7f620e7ae0991b8b7
6Neel9 <nilmandal300@gmail.com>
2022-07-31 21:26:59 +0530

    initial commit

---
+++ src/Constants.js
@@ -0,0 +1,5 @@
+const emojis = ['🚀','🤣','💘','💋','👽','🤖','🤡','⚡'];
+const pair_emojis = [ ...emojis, ...emojis ];
+const initRevealState = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+export {emojis, pair_emojis, initRevealState};
